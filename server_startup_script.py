0e3edd7835abb0dee88adf2211da10c68886a755
Simppu <90044857+Simppu@users.noreply.github.com>
2024-06-29 01:15:38 +0300

    Update server_startup_script.py
    - Added server saying it restarts in 10 minutes!
    - Added server saying it successfully restarted!
    - Added a command to restart the server!

--- server_startup_script.py
+++ server_startup_script.py
@@ -9,18 +9,19 @@ import time
 
 read, write = os.pipe()
 begin_time = time.time()
+wait_time = 12*60*60
 
 def get_sh_or_bat():
    
     if platform.system() == 'Windows':
-        return Path('run.bat')
+        return Path('./run.bat')
     else:
         return Path('./run.sh')
 
 def start_server_process(startup_file):
 
     if platform.system() == 'Windows':
-        return Popen('run.bat', text=True, shell=True, stdin=read)
+        return Popen('./run.bat', text=True, shell=True, stdin=read)
     else:
         return Popen('sh ' + './run.sh',stdin=read, shell=True, text=True)
 
@@ -29,7 +30,9 @@ def restart_timeout(server_process, startup_file, kill_signal, restart_signal):
     while True:
         # The intervall in seconds
         # This means 12 hours
-        is_killed = kill_signal.wait(12*60*60)
+        is_killed = kill_signal.wait(wait_time - 10*60)
+        os.write(write, "say Server restarting in 10 minutes!\n".encode())
+        is_killed = kill_signal.wait(10*60)
         if is_killed:
             break
         restart_signal.set()
@@ -39,13 +42,15 @@ def restart_timeout(server_process, startup_file, kill_signal, restart_signal):
         server_process.wait()
         server_process = start_server_process(startup_file)
         begin_time = time.time()
+        sleep(20)
+        os.write(write, "say Server successfully restarted!\n".encode())
 
 def server_crash_thread(process, startup_file, kill_signal, restart_signal):
     while True:
         process.wait()
         if kill_signal.wait(5):
             break
-        elif (time.time() - begin_time) < (12*60*60):
+        elif (time.time() - begin_time) < (wait_time):
             process = start_server_process(startup_file)
 
 def main():
@@ -74,6 +79,8 @@ def main():
             server_restart_thread.join()
             crash_thread.join()
             break
+        elif i == 'restart':
+            os.write(write, "stop\n".encode())
         else:
             os.write(write, (i + '\n').encode())
         
